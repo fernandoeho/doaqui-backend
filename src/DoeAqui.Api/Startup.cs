8c687be68f78e52539585cf5337371fd509d6e46
fernando <fernando.eho@gmail.com>
2019-01-14 21:31:41 -0200

    Registering dbcontext

--- src/DoeAqui.Api/Startup.cs
+++ src/DoeAqui.Api/Startup.cs
@@ -2,22 +2,32 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DoeAqui.Infrastructure.Configuration;
+using DoeAqui.Infrastructure.Context;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace DoeAqui.Api
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
+        private readonly IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
         {
+            _configuration = configuration;
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var databaseSettings = _configuration.GetSection("Database").Get<DatabaseSettings>();
+            services.AddDbContext<DoeAquiContext>(options => options.UseSqlServer(databaseSettings.ConnectionString));
+        }
+
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
---
+++ src/DoeAqui.Infrastructure/Migrations/20190114232931_InitialMigration.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DoeAqui.Infrastructure.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(type: "varchar(255)", maxLength: 255, nullable: false),
+                    Email = table.Column<string>(type: "varchar(max)", nullable: false),
+                    Password = table.Column<string>(type: "varchar(max)", nullable: false),
+                    PasswordSalt = table.Column<string>(type: "varchar(max)", nullable: false),
+                    Phone = table.Column<string>(type: "varchar(25)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
