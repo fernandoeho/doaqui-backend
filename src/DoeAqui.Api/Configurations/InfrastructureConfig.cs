c3a84819726d29cf73f9394497ab3700420b3c8b
fernando <fernando.eho@gmail.com>
2019-01-24 19:08:41 -0200

    Adding user repository

--- src/DoeAqui.Api/Configurations/InfrastructureConfig.cs
+++ src/DoeAqui.Api/Configurations/InfrastructureConfig.cs
@@ -1,8 +1,10 @@
+using DoeAqui.Domain.AggregateModels.UserAggregate.Repository;
 using DoeAqui.Domain.Core.Bus;
 using DoeAqui.Domain.Interfaces;
 using DoeAqui.Infrastructure.Bus;
 using DoeAqui.Infrastructure.Configuration;
 using DoeAqui.Infrastructure.Context;
+using DoeAqui.Infrastructure.Repositories;
 using DoeAqui.Infrastructure.UoW;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -17,6 +19,8 @@ namespace DoeAqui.Api.Configurations
             services.AddScoped<IUnitOfWork, UnitOfWork>();
 
             services.AddScoped<IBus, InMemoryBus>();
+
+            services.AddScoped<IUserRepository, UserRepository>();
 
             var databaseSettings = configuration.GetSection("Database").Get<DatabaseSettings>();
             services.AddDbContext<DoeAquiContext>(options => options.UseSqlServer(databaseSettings.ConnectionString));
