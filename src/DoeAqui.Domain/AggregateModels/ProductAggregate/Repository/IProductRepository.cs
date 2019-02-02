76fabd7e7f5df427b48e1549a472eb8b66cd7de2
fernando <fernando.eho@gmail.com>
2019-02-02 14:46:41 -0200

    Adding product repository

---
+++ src/DoeAqui.Domain/AggregateModels/ProductAggregate/Repository/IProductRepository.cs
@@ -0,0 +1,9 @@
+using DoeAqui.Domain.Interfaces;
+
+namespace DoeAqui.Domain.AggregateModels.ProductAggregate.Repository
+{
+    public interface IProductRepository : IRepository<Product>
+    {
+
+    }
+}
