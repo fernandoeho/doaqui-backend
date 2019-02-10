ded7f671362442605d841989183d6490139885bc
fernando <fernando.eho@gmail.com>
2019-02-10 13:13:42 -0200

    Removing private from properties

--- src/DoeAqui.Application/ViewModels/Product/ProductViewModel.cs
+++ src/DoeAqui.Application/ViewModels/Product/ProductViewModel.cs
@@ -6,13 +6,13 @@ namespace DoeAqui.Application.ViewModels.Product
     public class ProductViewModel
     {
         public Guid Id { get; set; }
-        public string Title { get; private set; }
-        public string Description { get; private set; }
-        public int Quantity { get; private set; }
-        public string Size { get; private set; }
-        public int Status { get; private set; }
-        public int Freight { get; private set; }
-        public string ImageUrl { get; private set; }
-        public UserViewModel User { get; private set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int Quantity { get; set; }
+        public string Size { get; set; }
+        public int Status { get; set; }
+        public int Freight { get; set; }
+        public string ImageUrl { get; set; }
+        public UserViewModel User { get; set; }
     }
 }
